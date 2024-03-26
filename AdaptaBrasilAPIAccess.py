a84375a396b114934b3d2e7d6997cea226c0469c
Mauro Assis <assismauro@hotmail.com>
2024-03-26 09:40:29 -0300

    Documentação (README.md)

--- AdaptaBrasilAPIAccess.py
+++ AdaptaBrasilAPIAccess.py
@@ -1,11 +1,15 @@
 import urllib.request, json
 import argparse
+import re
 
 def get_command_line_arguments():
     parser = argparse.ArgumentParser(description='Programa para obter metadados da API do AdaptaBrasil.')
-    parser.add_argument('--url', type=str,
-                        default = 'https://sistema.adaptabrasil.mcti.gov.br/api/hierarquia',
-                        help='URL para obtenção das URLs para acesso aos dados do AdaptaBrasil via API.')
+    parser.add_argument('--base_url', type=str,
+                        default = 'https://sistema.adaptabrasil.mcti.gov.br/',
+                        help='URL base de uma versão do AdaptaBrasil.')
+    parser.add_argument('--schema', type=str,
+                        default='adaptabrasil',
+                        help='schema a ser usado (adaptabrasil, impactos_economicos).')
     parser.add_argument('--arquivo_saida', type=str,
                         default = 'adaptaBrasilAPIEstrutura.csv',
                         help='Nome do arquivo destino (csv).')
@@ -14,45 +18,62 @@ def get_command_line_arguments():
 def if_none(value):
     return value if value is not None else ''
 
+CLEANR = re.compile('<.*?>')
+
+def cleanhtml(raw_html):
+  cleantext = re.sub(CLEANR, '', raw_html)
+  return cleantext.strip()
+
 if __name__ == '__main__':
     args = get_command_line_arguments()
-
-    with urllib.request.urlopen(args.url) as url, open(args.arquivo_saida,mode='w', encoding='utf-8') as csv_file:
+    url_hierarchy = f"{args.base_url}/api/hierarquia/{args.schema}"
+    with urllib.request.urlopen(url_hierarchy) as url, open(args.arquivo_saida,mode='w', encoding='utf-8') as csv_file:
         indicators = json.load(url)
         s = '\ufeff' # BOM code to preserve diacritics em Excel
         s += 'id|nome|url_mostra_mapas_na_tela|url_obtem_dados_indicador|url_obtem_totais_evolucao_tendencia|url_faz_download_geometrias_dados|' \
              'descricao_simples|descricao_completa|nivel|proporcao_direta|indicador_pai|' \
              'anos|setor_estrategico|tipo_geometria|unidade_medida|cenarios\n'
         list_scenario = []
+        seps = {}
         for indicator in indicators:
-            years = indicator['years'].split(',') if indicator['years'] is not None else None
-            if indicator['scenarios'][0] is not None:
-                url_getmapdata = ''
-                url_gettotal_evolucao_tendencia = ''
-                url_download = ''
-                url_show_map_on_the_site = ''
-                list_scenario = []
+            years = None
+            if indicator['years'] is None:
+                pass
+            elif type(indicator['years']) is list:
+                years = indicator['years']
+            else:
+                years = indicator['years'].split(',')
+            if indicator['sep_description'] is not None:
+                seps[indicator['sep_id']] = cleanhtml(indicator['sep_description'])
+            url_getmapdata = ''
+            url_gettotal_evolucao_tendencia = ''
+            url_download = ''
+            url_show_map_on_the_site = ''
+            list_scenario = []
+            if indicator['scenarios'] is None:
+                pass
+            else:
                 for scenario in indicator['scenarios']:
-                    list_scenario.append(scenario)
-                if indicator['years'] is not None:
-                    # https://sistema.adaptabrasil.mcti.gov.br/5000/1/2015/null/BR/municipio/
-                    url_show_map_on_the_site = f"https://sistema.adaptabrasil.mcti.gov.br/{indicator['id']}/1/{years[0]}/null/BR/municipio/"
-                    # https://sistema.adaptabrasil.dev.apps.rnp.br/api/mapa-dados/BR/municipio/1000/2015/null
-                    url_getmapdata = f"https://sistema.adaptabrasil.mcti.gov.br/api/mapa-dados/BR/municipio/" \
-                                     f"{indicator['id']}/{years[0]}/null"
-                    # https://sistema.adaptabrasil.mcti.gov.br/api/total/BR/municipio/5000/2/null
-                    url_gettotal_evolucao_tendencia = f"https://sistema.adaptabrasil.mcti.gov.br/api/total/BR/municipio/" \
-                                                      f"{indicator['id']}/null/{years[0]}"
-                    # https://sistema.adaptabrasil.dev.apps.rnp.br/api/geometria/data/1000/BR/null/2015/municipio/SHPz
-                    url_download = f"https://sistema.adaptabrasil.dev.apps.rnp.br/api/geometria/data/{indicator['id']}/" \
-                                   f"BR/null/{years[0]}/municipio/SHPz"
+                    list_scenario.append(scenario['label'])
+            if years is not None and years != []:
+                # https://sistema.adaptabrasil.mcti.gov.br/5000/1/2015/null/BR/municipio/
+                url_show_map_on_the_site = f"{args.base_url}/{indicator['id']}/1/{years[0]}/null/BR/municipio/{args.schema}"
+                # https://sistema.adaptabrasil.dev.apps.rnp.br/api/mapa-dados/BR/municipio/1000/2015/null
+                url_getmapdata = f"https://sistema.adaptabrasil.mcti.gov.br/api/mapa-dados/BR/municipio/" \
+                                 f"{indicator['id']}/{years[0]}/null/{args.schema}"
+                # https://sistema.adaptabrasil.mcti.gov.br/api/total/BR/municipio/5000/2/null
+                url_gettotal_evolucao_tendencia = f"https://sistema.adaptabrasil.mcti.gov.br/api/total/BR/municipio/" \
+                                                  f"{indicator['id']}/null/{years[0]}/{args.schema}"
+                # https://sistema.adaptabrasil.dev.apps.rnp.br/api/geometria/data/1000/BR/null/2015/municipio/SHPz
+                url_download = f"https://sistema.adaptabrasil.dev.apps.rnp.br/api/geometria/data/{indicator['id']}/" \
+                               f"BR/null/{years[0]}/municipio/SHPz/{args.schema}"
 
                 s += f"{indicator['id']}|{indicator['name']}|"
                 s += f"{url_show_map_on_the_site}|{url_getmapdata}|{url_gettotal_evolucao_tendencia}|{url_download}|" \
                     f"{indicator['simple_description']}|{indicator['complete_description']}|{indicator['level']}|" \
                     f"{if_none(indicator['pessimist'])}|{if_none(indicator['indicator_id_master'])}|{if_none(indicator['years'])}|" \
-                    f"{if_none(indicator['sep_description'])}|{if_none(indicator['geometrytype']) if int(indicator['level']) > 1 else ''}|{if_none(indicator['measurement_unit'])}|" \
-                    f"{if_none(indicator['scenarios'])}".replace('\r', ' ').replace('\n', ' ')
+                    f"{if_none(seps[indicator['sep_id']])}|{if_none(indicator['geometrytype']) if int(indicator['level']) > 1 else ''}|{if_none(indicator['measurement_unit'])}|" \
+                    f"{if_none(list_scenario)}".replace('\r', ' ').replace('\n', ' ')
                 s += "\n"
         csv_file.write(s)
     print('Feito!')
--- README.md
+++ README.md
@@ -1,1 +1,146 @@
+# Como acessar a API da plataforma Adapta Brasil
+
+O que está aqui disponibilizado são recursos para se acessar os dados da plataforma usando-se scripts, automatizando a obtenção desses dados e possibilitando até que outros sites façam esse acesso de maneira dinâmica.
+
+Há um script Python, [AdaptaBrasilAPIAccess.py](AdaptaBrasilAPIAccess.py), que gera URLs exemplo de cada API existente, ou pode ser usado o CSV [adaptaBrasilAPIEstrutura.csv](adaptaBrasilAPIEstrutura.csv) como exemplo.
+
+As colunas do CSV gerado são separadas pelo caractere "|", em vez de vírgula ou ponto-e-vírgula como é usual. Isso porque esses caracteres aparecem nos textos da então optou-se pelo "|", que normalmente não é usado em textos.
+
+Quando abrir esse arquivo no Excel, indique o "|" como separador.
+
+## Iniciando
+
+Para acessar os dados do Adapta usam-se URLs que especificam qual o tipo de dado a ser obtidos e algum tipo de filtro. São essas as diferentes URLs possíveis:
+
+Os dados são retornados por default em formato JSON. Quando é especificado um formato, como no caso das URLs de download de dados, será feito um arquivo .zip contendo os dados no formato solicitado.
+
+https://sistema.adaptabrasil.mcti.gov.br/api/hierarquia/adaptabrasil: acessa a hierarquia de indicadores que compõe o site. Um exemplo do retornado por essa API pode ser visto [aqui](jsonsamples%2Fhierarchy.json).
+O parâmetro **adaptabrasil** especifica o conjunto de dados a ser acessado. Atualmente há dois conjuntos de dados: **Adapta Brasil** e **Riscos Climáticos**, que são selecionados por esse último parâmetro, respectivamente **adaptabrasil** e **riscos_climaticos**.
+
+## Colunas do CSV
+
+### id:
+id do ìndicador.
+
+### nome: 
+Nome do indicador.
+
+**url_mostra_mapas_na_tela**: Exibe o portal no browser de acordo com os parâmetros indicados.
+
+Exemplo: https://sistema.adaptabrasil.mcti.gov.br/5000/1/2015/null/BR/municipio/adaptabrasil: 
+
+Parâmetros:
+
+_Indicador_: id do indicador, conforme pode ser obtido na hierarquia (5000, no exemplo).
+
+Dado a ser exibido: formato na tela em que os dados serão exibidos (1, no exemplo). 
+
+    1: Mapa
+    2: Totais
+    3: Evolução
+    4: Tendência
+
+_Ano_: Ano a que deverão corresponder os dados exibidos (2015).
+
+_Cenário_: id do cenário a ser exibido. Os ids dos cenários possíveis estão indicados na hierarquia. 
+
+_Recorte_: recorte a que corresponderão os dados exibidos (BR - Brasil). Os recortes possíveis estão indicados na hierarquia.
+
+_Resolução_: resolução a que corresponderão os dados exibidos (municipio). As resoluções possíveis estão indicadas na hierarquia.
+
+_Conjunto de dados_: (adaptabrasil)
+
+**url_obtem_dados_indicador**: Obtem os dados de um indicador associados a um recorte e uma resolução.
+
+Exemplo: https://sistema.adaptabrasil.mcti.gov.br/api/mapa-dados/BR/municipio/1000/2015/null/adaptabrasil
+
+_Nome da API_: (fixo, mapa-dados).
+
+_Recorte_: recorte a que corresponderão os dados exibidos (BR - Brasil). Os recortes possíveis estão indicados na hierarquia.
+
+_Resolução_: resolução a que corresponderão os dados exibidos (municipio). As resoluções possíveis estão indicadas na hierarquia.
+
+_Indicador_: id do indicador, conforme pode ser obtido na hierarquia (1000, no exemplo).
+
+_Ano_: Ano a que deverão corresponder os dados exibidos (2015).
+
+_Cenário_: id do cenário a ser exibido (null, no exemplo). Os ids dos cenários possíveis estão indicados na hierarquia. 
+
+_Conjunto de dados_: (adaptabrasil)
+
+**url_obtem_totais_evolucao_tendencia**: Obtém dados por faixa de valores de um determinado indicador monstrados nas telas de Totais, Evolução e Tendência do Adapta Brasil:
+
+Exemplo: https://sistema.adaptabrasil.mcti.gov.br/api/total/BR/municipio/1000/null/2015/adaptabrasil: 
+
+_Nome da API_: (fixo, _total_).
+
+_Recorte_: recorte a que corresponderão os dados exibidos (BR - Brasil). Os recortes possíveis estão indicados na hierarquia.
+
+_Resolução_: resolução a que corresponderão os dados exibidos (municipio). As resoluções possíveis estão indicadas na hierarquia.
+
+_Indicador_: id do indicador, conforme pode ser obtido na hierarquia (1000, no exemplo).
+
+_Cenário_: id do cenário a ser exibido (null, no exemplo). Os ids dos cenários possíveis estão indicados na hierarquia. 
+
+_Ano_: Ano a que deverão corresponder os dados exibidos (2015).
+
+_Conjunto de dados_: (adaptabrasil)
+
+**url_faz_download_geometrias_dados**: faz o download de geometrias com seus dados associados, em diversos formatos:
+https://sistema.adaptabrasil.dev.apps.rnp.br/api/geometria/data/1000/BR/null/2015/municipio/SHPz/adaptabrasil
+
+_Nome da API_: (fixo, _geometria/data_).
+
+_Indicador_: id do indicador, conforme pode ser obtido na hierarquia (1000, no exemplo).
+
+_Recorte_: recorte a que corresponderão os dados exibidos (BR - Brasil). Os recortes possíveis estão indicados na hierarquia.
+
+_Cenário_: id do cenário a ser exibido (null, no exemplo). Os ids dos cenários possíveis estão indicados na hierarquia. 
+
+_Ano_: Ano a que deverão corresponder os dados exibidos (2015).
+
+_Resolução_: resolução a que corresponderão os dados exibidos (municipio). As resoluções possíveis estão indicadas na hierarquia.
+
+Formatos geoespaciais disponíveis:
+
+    SHPz    : shapefile (arquivo zip contendo os vários arquivos que compõe o formato).
+    GEOJSONz: geoJSON
+    KMZz    : formato compatível com o Google Maps
+
+Formatos tabulares disponíveis:
+
+    JSONz: JSON
+    XLSXz: planilha Excel
+    CSV  :   CSV (texto com colunas separadas por ';')
+
+Formato de imagem disponível:
+
+    PNG: png
+
+_Conjunto de dados_: (adaptabrasil)
+
+**descricao_simples**: descrição simplificada do que os dados desse indicador representam.
+
+**descricao_completa**: descrição detalhada do indicador.
+
+**nivel**: nível do indicador na hierarquia que os agrupa. Só os indicadores de nívem maior que 1 possuem dados associados a eles.
+
+**proporcao_direta**: indica se quanto maior o valor pior ou melhor o significado dele.
+
+Valores:
+
+    0: indica que valores maiores significam uma situação pior.
+    1: indica que valores maiores significam uma situação melhor.
+
+**indicador_pai**: os indicadores formam uma hierarquia. Os indicadores de nivel "1", que correspondem aos Setores Estratégicos, agrupam alguns dos de nível "2" e assim por diante, e essa coluna expressa essa relação.
+
+**anos**: anos para os quais há valores desse indicador.
+
+**setor_estrategico**: setor estratégico ao qual pertence o indicador.
+
+**tipo_geometria**: tipo das geometrias usadas nesse indicador: Multipolygon, MultilineString, MultiPoint.
+
+**unidade_medida**: unidade de medida dos valores do indicador.
+
+**cenários**: cenários possíveis para esse indicador.
 
